c5714448a9bed06d07edb317345471bdc7a362c8
omars154 <omarsaifan154@gmail.com>
2025-04-27 22:49:48 +0300

    init react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
-import './App.css';
+import React, { useState } from 'react';
+import ContactForm from './components/contactForm';
+import ContactList from './components/contactList';
 
 function App() {
-  const [count, setCount] = useState(0);
-  function addNumbers () {
-    setCount(count+1);
-  }
+  const [contacts, setContacts] = useState([]);
+
+  const addContact = (newContact) => {
+    setContacts([...contacts, newContact]);
+  };
+
+  const delet = (id) => {
+    setContacts(contacts.filter(id));
+  };
+
   return (
-    <div className="App">
-      <p>The count rn is {count}</p>
-      <button onClick={addNumbers}>add numbers</button>
+    <div>
+      <h1>Contact Manager</h1>
+      <ContactForm addContact={addContact} />
+      <ContactList contacts={contacts} deleteContact={delet} />
     </div>
   );
 }
